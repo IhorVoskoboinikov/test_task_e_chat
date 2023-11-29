b837f146b1c58e8eb3aabb8a8ce6242c2e7e7222
YOUR_NAME <YOUR_EMAIL>
2023-11-29 16:52:26 +0200

    added logic if we get "send" from RabbitMQ

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+import os
 import threading
+import requests
+import json
 
 from bot_run import bot, get_last_message
 from queue_producer import QueueProducer
 
 
-def callback(ch, method, properties, body):
+def callback(ch, method, properties, body) -> None:
     body = body.decode('utf-8')
+    last_message = get_last_message()
+
     if body == "send":
         print(f" [queue] Received from RabbitMQ: {body}")
+
+        external_api_url = os.getenv('EXTERNAL_API_URL')
+        if external_api_url:
+            payload = {'last_message': last_message}
+            headers = {'Content-Type': 'application/json'}
+
+            try:
+                response = requests.post(external_api_url, data=json.dumps(payload), headers=headers)
+                response.raise_for_status()
+                print(f" [queue] Status: {response.status_code}")
+                print(f" [queue] POST request sent to {external_api_url}")
+            except requests.exceptions.RequestException as e:
+                print(f" [queue] Error sending POST request: {e}")
+        else:
+            print(" [queue] EXTERNAL_API_URL is not defined in the environment variables.")
+
     elif body == "print":
-        print(f" [queue] Received from RabbitMQ: {body}. Last message from chat bot: {get_last_message()}")
+        print(f" [queue] Received from RabbitMQ: {body}. Last message from chat bot: {last_message}")
     else:
-        print("error!")
+        print("Error! Unknown command!")
 
 
 conn = QueueProducer()
