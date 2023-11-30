1615394498f1cbc701c1effa870ded170a5683b6
YOUR_NAME <YOUR_EMAIL>
2023-11-30 22:07:14 +0200

    added send_last_message and print_last_message (using in callback)

--- main.py
+++ main.py
@@ -3,34 +3,41 @@ import threading
 import requests
 import json
 
-from bot_run import bot, get_last_message
+import bot
 from queue_producer import QueueProducer
+
+
+def send_last_message(last_message, body):
+    print(f" [queue] Received from RabbitMQ: {body}")
+
+    external_api_url = os.getenv('EXTERNAL_API_URL')
+    if external_api_url:
+        payload = {'last_message': last_message}
+        headers = {'Content-Type': 'application/json'}
+
+        try:
+            response = requests.post(external_api_url, data=json.dumps(payload), headers=headers)
+            response.raise_for_status()
+            print(f" [queue] Status: {response.status_code}")
+            print(f" [queue] POST request sent to {external_api_url}")
+        except requests.exceptions.RequestException as e:
+            print(f" [queue] Error sending POST request: {e}")
+    else:
+        print(" [queue] EXTERNAL_API_URL is not defined in the environment variables.")
+
+
+def print_last_message(last_message, body):
+    print(f" [queue] Received from RabbitMQ: {body}. Last message from chat bot: {last_message}")
 
 
 def callback(ch, method, properties, body) -> None:
     body = body.decode('utf-8')
-    last_message = get_last_message()
+    last_message = bot.last_message
 
     if body == "send":
-        print(f" [queue] Received from RabbitMQ: {body}")
-
-        external_api_url = os.getenv('EXTERNAL_API_URL')
-        if external_api_url:
-            payload = {'last_message': last_message}
-            headers = {'Content-Type': 'application/json'}
-
-            try:
-                response = requests.post(external_api_url, data=json.dumps(payload), headers=headers)
-                response.raise_for_status()
-                print(f" [queue] Status: {response.status_code}")
-                print(f" [queue] POST request sent to {external_api_url}")
-            except requests.exceptions.RequestException as e:
-                print(f" [queue] Error sending POST request: {e}")
-        else:
-            print(" [queue] EXTERNAL_API_URL is not defined in the environment variables.")
-
+        send_last_message(last_message=last_message, body=body)
     elif body == "print":
-        print(f" [queue] Received from RabbitMQ: {body}. Last message from chat bot: {last_message}")
+        print_last_message(last_message=last_message, body=body)
     else:
         print("Error! Unknown command!")
 
@@ -40,7 +47,7 @@ conn.open_connection()
 conn.basic_consume(callback)
 
 if __name__ == "__main__":
-    tr_1 = threading.Thread(target=bot.polling, kwargs={"none_stop": True})
+    tr_1 = threading.Thread(target=bot.bot_run)
     tr_2 = threading.Thread(target=conn.channel.start_consuming)
     print(' [queue] Waiting for messages from RabbitMQ.')
 
