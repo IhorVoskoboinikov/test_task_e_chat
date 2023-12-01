3cbac994fad76681464b3e7cbb9356c96707acf1
YOUR_NAME <YOUR_EMAIL>
2023-12-01 13:46:58 +0200

    update main from sync to async version

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
+import asyncio
+import json
 import os
-import threading
 from typing import Any
 
-import requests
-import json
+import aio_pika
+import aiohttp
+from dotenv import load_dotenv
+from queue_producer import AsyncQueueProducer
 
 import bot
-from queue_producer import QueueProducer
+
+load_dotenv()
 
 
-def send_last_message(last_message: str, body: str) -> None:
+async def send_last_message(last_message: str, body: str) -> None:
     print(f" [queue] Received from RabbitMQ: {body}")
 
     external_api_url = os.getenv('EXTERNAL_API_URL')
@@ -18,43 +22,60 @@ def send_last_message(last_message: str, body: str) -> None:
         headers = {'Content-Type': 'application/json'}
 
         try:
-            response = requests.post(external_api_url, data=json.dumps(payload), headers=headers)
-            response.raise_for_status()
-            print(f" [queue] Status: {response.status_code}")
-            print(f" [queue] POST request sent to {external_api_url}")
-        except requests.exceptions.RequestException as e:
+            async with aiohttp.ClientSession() as session:
+                async with session.post(external_api_url, data=json.dumps(payload), headers=headers) as response:
+                    response.raise_for_status()
+                    print(f" [queue] Status: {response.status}")
+                    print(f" [queue] POST request sent to {external_api_url}")
+        except aiohttp.ClientError as e:
             print(f" [queue] Error sending POST request: {e}")
     else:
         print(" [queue] EXTERNAL_API_URL is not defined in the environment variables.")
 
 
-def print_last_message(last_message: str, body: str) -> None:
+async def print_last_message(last_message: str, body: str) -> None:
     print(f" [queue] Received from RabbitMQ: {body}. Last message from chat bot: {last_message}")
 
 
-def callback(ch: Any, method: Any, properties: Any, body: bytes) -> None:
+async def callback(ch: Any, method: Any, properties: Any, body: bytes) -> None:
     body = body.decode('utf-8')
     last_message = bot.last_message
 
     if body == "send":
-        send_last_message(last_message=last_message, body=body)
+        await send_last_message(last_message=last_message, body=body)
     elif body == "print":
-        print_last_message(last_message=last_message, body=body)
+        await print_last_message(last_message=last_message, body=body)
     else:
         print("Error! Unknown command!")
 
 
-conn = QueueProducer()
-conn.open_connection()
-conn.basic_consume(callback)
+async def process_message(message: aio_pika.abc.AbstractIncomingMessage):
+    async with message.process():
+        data = message.body.decode('utf-8')
+        last_message = bot.last_message
+        print(f"Received message from RabbitMQ: {data}")
+        if data == "send":
+            await send_last_message(last_message=last_message, body=data)
+        elif data == "print":
+            await print_last_message(last_message=last_message, body=data)
+        else:
+            print("Error! Unknown command!")
+
+
+async def queue_run() -> None:
+    print(' [queue] Waiting for messages from RabbitMQ.')
+    conn = AsyncQueueProducer()
+    connection = await conn.open_connection()
+    await conn.basic_consume(callback=process_message)
+    try:
+        await asyncio.Future()
+    finally:
+        await connection.close()
+
+
+async def main():
+    await asyncio.gather(bot.bot_run(), queue_run())
+
 
 if __name__ == "__main__":
-    tr_1 = threading.Thread(target=bot.bot_run)
-    tr_2 = threading.Thread(target=conn.channel.start_consuming)
-    print(' [queue] Waiting for messages from RabbitMQ.')
-
-    tr_1.start()
-    tr_2.start()
-
-    tr_1.join()
-    tr_2.join()
+    asyncio.run(main())
