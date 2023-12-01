a372da7e798169a76289780f04b6abdd4479d5a9
YOUR_NAME <YOUR_EMAIL>
2023-12-01 13:46:02 +0200

    update chat_bot to async with aiogram

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
+import asyncio
 import os
-
-import telebot
 from dotenv import load_dotenv
+
+from aiogram import Bot, Dispatcher, types, F
+from aiogram.filters.command import Command
 
 load_dotenv()
 
 TOKEN = os.getenv("BOT_TOKEN")
-print(' [chat_bot] Chat Bot is running...')
-bot = telebot.TeleBot(TOKEN)
+
+bot = Bot(token=TOKEN)
+
+dp = Dispatcher()
 
 last_message = None
 
@@ -16,24 +20,22 @@ def get_last_message() -> str | None:
     return last_message
 
 
-@bot.message_handler(commands=["start"])
-def inline(message) -> None:
-    bot.send_message(
-        message.chat.id,
-        " [chat_bot] Welcome to our telegram bot! \n\nYou can send messages to the chat bot!"
-    )
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message) -> None:
+    await message.answer(" [chat_bot] Welcome to our telegram bot!\n\nYou can send messages to the chat bot!")
 
 
-@bot.message_handler(content_types=["text"])
-def handle_text_messages(message) -> None:
+@dp.message(F.text)
+async def handle_text_messages(message: types.Message) -> None:
     global last_message
     last_message = message.text
     print(f" [chat_bot] Chat bot received message from: ID - {message.chat.id}! Message: {last_message}")
 
 
-def bot_run():
-    bot.polling(none_stop=True)
+async def bot_run() -> None:
+    print(' [chat_bot] Chat Bot is running...')
+    await dp.start_polling(bot)
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+if __name__ == "__main__":
+    asyncio.run(bot_run())
