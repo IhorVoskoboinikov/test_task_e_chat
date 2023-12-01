2fd1522f05fc9c7a0a777c5565abda19cfa99057
YOUR_NAME <YOUR_EMAIL>
2023-12-01 13:47:13 +0200

    update from sync to async version

--- producer.py
+++ producer.py
@@ -1,22 +1,24 @@
+import asyncio
+
 from dotenv import load_dotenv
-from queue_producer import QueueProducer
+from queue_producer import AsyncQueueProducer
 
 load_dotenv()
 
 
-def main():
-    conn = QueueProducer()
-    conn.open_connection()
+async def main() -> None:
+    conn = AsyncQueueProducer()
+    await conn.open_connection()
     while True:
-        message_from_user = input("Enter command 'send' to RabbitMQ (or 'exit' to quit): ")
+        message_from_user = input("Enter command 'send' or 'print' to RabbitMQ (or 'exit' to quit): ")
         if message_from_user.lower() == 'exit':
             break
         elif message_from_user.lower() not in ["print", 'send']:
             print("You can only add a 'print' or 'send' command to the queue!\nTry again!")
             continue
 
-        conn.add_to_queue(message_from_user)
+        await conn.add_to_queue(message_from_user)
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
